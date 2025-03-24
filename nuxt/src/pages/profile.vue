db2bdca6c69eb54c5fb789b81356dcab34d9502d
Victor Osipov <victorosipovprogrammer@gmail.com>
2025-03-24 15:41:33 +0300

    added profile page

--- nuxt/src/pages/profile.vue
+++ nuxt/src/pages/profile.vue
@@ -1,7 +1,25 @@
 <template>
-    <div class="container mx-auto">
-        <h1 class="mb-2">Профиль</h1>
-        <AppButton @click="logout" class="bg-sky-300">Выйти</AppButton>
+    <div class="container mx-auto ">
+        <h1 class="mb-4 text-2xl">Профиль</h1>
+
+        <div class="p-2 rounded-md bg-pink-50">
+            <div class="flex justify-between items-center">
+                <div class="flex mb-4 items-center gap-4">
+                    <img v-if="user_store.user?.avatar" class="rounded-full h-20" :src="user_store.user?.avatar" alt="">
+                    <img v-else class="rounded-full h-20" src="@/shared/img/avatar.avif" alt="avatar">
+                    
+                    <div>
+                        <p class="text-2xl">{{ user_store.user?.first_name ?? 'Имя' }}</p>
+                        <p class="text-2xl">{{ user_store.user?.last_name ?? 'Фамилия' }}</p>
+                    </div>
+                </div>
+                
+                <AppButton v-if="user_store.is_login" @click="logout" class="bg-sky-400 max-h-[2rem] text-white">Выйти</AppButton>
+            </div>
+
+            <p>{{ user_store.user?.email ?? 'Email' }}</p>
+            <!-- <p>{{ user_store.user. }}</p> -->
+        </div>
     </div>
 
 </template>
